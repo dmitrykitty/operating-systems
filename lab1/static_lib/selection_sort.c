7e3135f3597baeb9fb7edd3b9dc0166a64da1e7c
Dmitry_Kitty <dnikitin@student.agh.edu.pl>
2026-03-15 23:15:14 +0100

    gitignore added:

--- lab1/static_lib/selection_sort.c
+++ lab1/static_lib/selection_sort.c
@@ -17,15 +17,3 @@ void selection_sort(float* num,int n){
     }
 }
 
-void bubble_sort(float* num, int n){
-    for(int i = 0; i < n - 1; i++){
-        for(int j = 0; j < n - i - 1; j++){
-            if(num[j] > num[j + 1]){
-                float temp = num[j];
-                num[j] = num[j + 1];
-                num[j + 1] = temp;
-            }
-        }
-    }
-}
-
