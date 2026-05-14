a2924b3ea08836709add4b7881e33787cc5011ac
Dmitry_Kitty <dnikitin@student.agh.edu.pl>
2026-05-14 19:13:48 +0200

    numeral system convertation added

---
+++ c_practise/01_basics/5_problem_numeral_system.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_converted(unsigned x, unsigned y){
+    size_t sz = 1; 
+    unsigned y_check = y; 
+    while(y_check < x){
+        sz++; 
+        y_check *= y; 
+    }
+
+    unsigned* arr = (unsigned*)malloc(sz * sizeof(unsigned)); 
+    if(arr == NULL){
+        perror("malloc"); 
+    }
+
+    for(int i = sz - 1; i >= 0; i--){
+        arr[i] = x % y; 
+        x /= y; 
+    }
+
+    for(int i = 0; i < sz; i++){
+        printf("%u", arr[i]);
+    }
+}
+
+
+int main(){
+    unsigned int x, y; 
+    int ic = scanf("%u%u", &x, &y); 
+    print_converted(x, y); 
+}
